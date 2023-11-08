9fa528bae272234a05a1c3678be0caca5c37e569
zalven-official <zalvendayao888@gmail.com>
2023-11-09 02:24:18 +0800

    fix icon

--- client/components/profile/ProfileCompletion.vue
+++ client/components/profile/ProfileCompletion.vue
@@ -25,23 +25,23 @@
           your chances of landing into desired role</p>
         <ul class="list-disc text-xs opacity-70 mt-5 list-outside">
           <li class="flex items-center mt-2">
-            <global-icon icon="fa-regular fa-circle" class="text-right w-3 h-3 mr-3" />
+            <icon icon="fa-regular fa-circle" class="text-right w-3 h-3 mr-3" />
             <p>Share <span class="text-primary">Date of Birth</span></p>
           </li>
           <li class="flex items-center mt-2">
-            <global-icon icon="fa-regular fa-circle" class="text-right w-3 h-3 mr-3" />
+            <icon icon="fa-regular fa-circle" class="text-right w-3 h-3 mr-3" />
             <p>Specifiy <span class="text-primary">Current Monthly Salary</span></p>
           </li>
           <li class="flex items-center mt-2">
-            <global-icon icon="fa-regular fa-circle" class="text-right w-3 h-3 mr-3" />
+            <icon icon="fa-regular fa-circle" class="text-right w-3 h-3 mr-3" />
             <p>Add one (1) <span class="text-primary">Character Reference</span></p>
           </li>
           <li class="flex items-center mt-2">
-            <global-icon icon="fa-regular fa-circle" class="text-right w-3 h-3 mr-3" />
+            <icon icon="fa-regular fa-circle" class="text-right w-3 h-3 mr-3" />
             <p>Add <span class="text-primary">Current Address</span></p>
           </li>
           <li class="flex items-center mt-2">
-            <global-icon icon="fa-regular fa-circle" class="text-right w-3 h-3 mr-3" />
+            <icon icon="fa-regular fa-circle" class="text-right w-3 h-3 mr-3" />
             <p>Add atleast five (5) items in <span class="text-primary">Other Skills</span></p>
           </li>
         </ul>
